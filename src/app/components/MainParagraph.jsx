2904e6159dcf9ca07122a2e939075a95aa98cf01
amineelakkermi <amineelakkermi37@gmail.com>
2025-09-19 13:01:28 +0100

    mise a jour

--- src/app/components/MainParagraph.jsx
+++ src/app/components/MainParagraph.jsx
@@ -3,15 +3,18 @@ import { textVariant } from "../styles/motion";
 import styles from "../styles/style";
 import { motion } from 'framer-motion'
 
+{/*
+variants={textVariant(0.4)}
+  initial='hidden'
+  whileInView='show'  
+*/}
 
 export default function MainParagraph({ text }) {
   return (
-  <motion.p
-  variants={textVariant(0.4)}
-  initial='hidden'
-  whileInView='show'
+  <p
+  
   className={`${styles.paragraph} mb-8`}>
    {text}
-  </motion.p>
+  </p>
   );
 }
--- src/app/components/MainTitle.jsx
+++ src/app/components/MainTitle.jsx
@@ -3,17 +3,19 @@ import { textVariant } from "../styles/motion";
 import styles from "../styles/style";
 import { motion } from 'framer-motion'
 
-
+ {/*
+     variants={textVariant(0.3)}
+    initial='hidden'
+    whileInView='show'  
+    */}
 export default function MainTitle({ title, span, restTitle }) {
   return (
-    <motion.h1
-    variants={textVariant(0.3)}
-    initial='hidden'
-    whileInView='show'
+    <h1
+   
     className={`${styles.title} mb-6 text-primary`}>
       {title}
       {span && <span className="text-blue-400">{span}</span>}
       {restTitle}
-    </motion.h1>
+    </h1>
   );
 }
